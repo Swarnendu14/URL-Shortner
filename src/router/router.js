8c79ae9752975076c830f23a77d6bd5d51bf0b79
Swarnendu14 <swarnendustps@gmail.com>
2023-05-31 16:18:25 +0530

    Common Model of project

---
+++ src/router/router.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const urlController=require('../Controllers/URLController');
+const mid=require('../middlewares/middleware');
+const Router=express.Router();
+
+Router.post('/url/shorten',);
+Router.get('/:urlCode',);
+
+module.exports=Router;
