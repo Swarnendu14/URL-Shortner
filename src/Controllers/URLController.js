a9f6699c4a51a9f39322181f730e2e0c560e5fba
Swarnendu14 <swarnendustps@gmail.com>
2023-06-02 18:05:25 +0530

    Cacheing added

--- src/Controllers/URLController.js
+++ src/Controllers/URLController.js
@@ -1,5 +1,24 @@
 const URLmodel = require('../model/URLmodel.js');
 const {customAlphabet}= require('nanoid');
+const redis = require("redis");
+
+const { promisify } = require("util");
+
+//1. Connect to the redis server
+const redisClient = redis.createClient(
+  16834,
+  "redis-16834.c264.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("dW2yO03aBkWYV7Vr0AmK1d6Kj3nvzyCT", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 
 const shortURL = async (req,res)=>  {
@@ -34,10 +53,9 @@ const shortURL = async (req,res)=>  {
                 shortUrl: shortURL,
                 urlCode: urlCode
             } 
-
+            await SET_ASYNC(`${data.urlCode}`,data.longUrl);
             const createShortUrl = await URLmodel.create(data);
             res.status(201).json({status: true, "data": data})
-
         }
 
     } catch(err) {
@@ -48,14 +66,16 @@ const shortURL = async (req,res)=>  {
 const showURL = async (req,res)=>  {
     try {
         const URLcode = req.params.urlCode;
-
+        let cahcedData = await GET_ASYNC(`${URLcode}`);
+        
+        if(cahcedData) {
+            return res.status(302).redirect(cahcedData);
+        }
         const data = await URLmodel.findOne({urlCode: URLcode})
         if (!data) {
-            return res.status(404).json({status: false, message: "Invalid URL"});
+            return res.status(404).json({status: false, message: "Invalid URLcode"});
         }
-        
         res.status(302).redirect(data.longUrl);
-
     } catch(err) {
         res.status(500).json({status: false, message:err.message});
     }
